678c2ef8b37a0bfdb09814c49ac30611ab170816
Mike <m.umyskov@list.ru>
2018-01-10 00:53:46 +0300

    usages

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 
 namespace Program1;
 
+use Program1\Classes\Content;
+use BracketsParser\BracketsParser;
+
 require_once __DIR__ . "/vendor/autoload.php";
 $filename = __DIR__ . "/data/data.txt";
-if (\BracketsParser\BracketsParser::run(Classes\Content::get($filename))) {
+if (BracketsParser::run(Content::get($filename))) {
     echo 'Строка валидна';
 } else {
     echo 'Строка невалидна';
