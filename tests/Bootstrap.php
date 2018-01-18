9d53f15e4f2b81faa33e327242cce861c9bf536a
Mike <m.umyskov@list.ru>
2018-01-18 14:29:13 +0300

    PHPUnit

---
+++ tests/Bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mike
+ * Date: 18.01.2018
+ * Time: 14:09
+ */
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
---
+++ tests/FirstTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mike
+ * Date: 02.01.2018
+ * Time: 15:02
+ */
+
+use PHPUnit\Framework\TestCase;
+use BracketsParser\BracketsParser;
+
+class MyTest extends TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(BracketsParser::run('()'));
+    }
+    public function testFalse()
+    {
+        $this->assertFalse(BracketsParser::run('())'));
+    }
+}
