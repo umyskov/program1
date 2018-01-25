6c9515d87a4a6c5897e35b9aaf5c03aba0c86722
Mike <m.umyskov@list.ru>
2018-01-25 22:56:29 +0300

    Code

--- src/Classes/SocketServer.php
+++ src/Classes/SocketServer.php
@@ -39,58 +39,52 @@ class SocketServer
         $address = $this->server;
         $port = $this->port;
 
-        if (($sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === false) {
+        if (($sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === false){
             echo "socket_create() failed: reason: " . socket_strerror(socket_last_error()) . "\n";
         }
 
-        if (socket_bind($sock, $address, $port) === false) {
+        if (socket_bind($sock, $address, $port) === false){
             echo "socket_bind() failed: reason: " . socket_strerror(socket_last_error($sock)) . "\n";
         }
 
-        if (socket_listen($sock, 5) === false) {
+        if (socket_listen($sock, 5) === false){
             echo "socket_listen() failed: reason: " . socket_strerror(socket_last_error($sock)) . "\n";
         }
 
         do {
-
-            if (($msgsock = socket_accept($sock)) === false) {
+            if (($msgsock = socket_accept($sock)) === false){
                 echo "socket_accept() failed: reason: " . socket_strerror(socket_last_error($sock)) . "\n";
                 break;
-
             } else {
                 $pid = pcntl_fork();
             }
 
-            if ($pid == 0) {
+            if ($pid == 0){
                 /* Send instructions. */
                 $msg = "\nWelcome to the PHP Test Server. \n" .
                     "To quit, type 'quit'. To shut down the server type 'shutdown'.\n";
                 socket_write($msgsock, $msg, strlen($msg));
 
                 do {
-                    if (false === ($buf = socket_read($msgsock, 2048, PHP_NORMAL_READ))) {
+                    if (false === ($buf = socket_read($msgsock, 2048, PHP_NORMAL_READ))){
                         echo "socket_read() failed: reason: " . socket_strerror(socket_last_error($msgsock)) . "\n";
                         break 2;
                     }
 
-                    if (!$buf = trim($buf)) {
+                    if (!$buf = trim($buf)){
                         continue;
                     }
 
-                    if ($buf == 'quit') {
+                    if ($buf == 'quit'){
                         break;
-                    }
-
-                    if ($buf == 'shutdown') {
-                        socket_close($msgsock);
-                        break 2;
                     }
 
                     $response = $task($buf);
                     socket_write($msgsock, $response, strlen($response));
                 } while (true);
+                $message = "Bye!\n";
+                socket_write($msgsock, $message, strlen($message));
                 socket_close($msgsock);
-
                 exit(1);
             }
 
