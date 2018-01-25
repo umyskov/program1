4542d6667f0f557c7e81fd231cb939c116016ecd
Mike <m.umyskov@list.ru>
2018-01-25 18:50:45 +0300

    SocketServer

--- index.php
+++ index.php
@@ -1,14 +1,30 @@
+#!/usr/local/bin/php -q
 <?php
 
 namespace Program1;
 
-use Program1\Classes\Content;
-use BracketsParser\BracketsParser;
+//error_reporting(E_ALL);
+//set_time_limit(0);
 
-require_once __DIR__ . "/vendor/autoload.php";
-$filename = __DIR__ . "/data/data.txt";
-if (BracketsParser::run(Content::get($filename))) {
-    echo 'Строка валидна';
-} else {
-    echo 'Строка невалидна';
+use BracketsParser\BracketsParser;
+use Program1\Classes\SocketServer;
+
+//use Program1\Classes\Content;
+
+$pid = pcntl_fork();
+
+if ($pid == 0) {
+    require_once __DIR__ . "/vendor/autoload.php";
+    //$filename = __DIR__ . "/data/data.txt";
+
+    $task = function ($string) {
+        return (BracketsParser::run($string/*Content::get($filename)*/) === true ? '' : 'In')
+            . "Valid string\n";
+
+    };
+    (new SocketServer('127.0.0.1'))
+        ->port('fromCommandLine')
+        ->startAndWait($task);
+    exit(1);
 }
+
