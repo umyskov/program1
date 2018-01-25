47c0e34b8e1b5e683f2bb4c29182207b027015e2
Mike <m.umyskov@list.ru>
2018-01-25 18:51:33 +0300

    Exceptions

---
+++ src/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 25.01.18
+ * Time: 13:15
+ */
+
+namespace Program1\Exceptions;
+
+
+class InvalidArgumentException extends \Exception
+{
+
+}
